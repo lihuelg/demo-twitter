6ac88340806e8088797f311ae26453f9078d95e9
Lautaro Galarza <lautarogalarza@gmail.com>
2019-06-11 16:08:09 +0200

    Add formatted date to the feed list & sort it by date

--- src/feed/sagas/generators.js
+++ src/feed/sagas/generators.js
@@ -1,4 +1,4 @@
-import { take, put, getContext, fork } from 'redux-saga/effects';
+import { take, put, getContext, fork, select } from 'redux-saga/effects';
 import { eventChannel } from 'redux-saga';
 import {
   updateFeed,
@@ -6,13 +6,20 @@ import {
   addPostSuccess,
   addPostError,
 } from '../actions';
-import errorParser from '../../shared/utils/errorParser';
+import { postDateFromTimestamp, errorParser } from '../../shared/utils';
 
 function createGetPostChannel(firebaseCollection) {
   return eventChannel(emit => {
 
     const onValueHandler = snapshot => {
-      emit({ data: snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) || [] });
+      emit({ 
+        data: snapshot.docs
+        .map(doc => {
+          const data = doc.data()
+          return { id: doc.id, ...data, createdAtFormatted: postDateFromTimestamp(data.createdAt)};
+        })
+        .sort((postA, postB) => postB.createdAt.toDate() - postA.createdAt.toDate()) 
+        || [] });
     }
 
     const onErrorHandler = errorEvent => {
@@ -44,7 +51,8 @@ export function* requestFeed({ user }){
 export function* addPost({ post: message }){
   try{
     const { posts } = yield getContext('firebase');
-    const item = yield posts().add({ message, createdBy: 'bGF5359vgNFpaQBS4ORG' });
+    const userId = yield select(state => state.authReducer.id);
+    const item = yield posts().add({ message, createdBy: userId, createdAt: new Date() });
     yield put(addPostSuccess(item));
   }catch(error){
     console.error(error);
