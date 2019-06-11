ab94cafc8577095de47c7298e81ad73caa7181ba
Lautaro Galarza <lautarogalarza@gmail.com>
2019-06-11 14:04:07 +0200

    Add saga channel to listen for authstate changes

--- src/auth/sagas/generators.js
+++ src/auth/sagas/generators.js
@@ -1,14 +1,49 @@
-import { put, call, getContext } from 'redux-saga/effects';
+import { put, call, fork, take, getContext } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
 import * as actionTypes from '../constants/actionTypes';
 import {
+  updateAuth,
   requestAuthError,
   requestRegisterError,
 } from '../actions';
 import errorParser from '../../shared/utils/errorParser';
 
-function request(successCb, errorCb) {
-  return function* ({ username, password, history, type }) {
+function createAuthChangedChannel(auth) {
+  return eventChannel(emit => {
+
+    const onValueHandler = user => {
+      console.log(user);
+      emit({ data: { token: user.ra, id: user.uid }});
+    }
+
+    const onErrorHandler = errorEvent => {
+      emit(new Error(errorEvent));
+    }
+
+    const unsubscribe = auth.onAuthStateChanged(onValueHandler, onErrorHandler);
+
+    return () => unsubscribe()
+  })
+}
+
+function* checkAuthUpdates(){
+  const { auth } = yield getContext('firebase');
+    const getAuthChannel = createAuthChangedChannel(auth);
+    try{
+      while(true){
+       const { data } = yield take(getAuthChannel);
+       yield put(updateAuth(data)) 
+      }
+    }catch(error){
+      console.error(error);
+      yield put(requestAuthError(errorParser(error)));
+    }
+}
+
+function request(errorCb) {
+  return function* ({ username, password, type, history }) {
     try {
+      yield fork(checkAuthUpdates);
       const { authenticate, register } = yield getContext('firebase');
       let apiCall;
       switch(type){
@@ -21,6 +56,7 @@ function request(successCb, errorCb) {
         default:
       }
       yield call(apiCall, { username, password });
+      history.push('/');
     } catch (error) {
       console.error(error);
       yield put(errorCb(errorParser(error)));
