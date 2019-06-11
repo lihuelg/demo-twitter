ed5120253e80bac74f4bb0c62a8de11d45715ae9
Lautaro Galarza <lautarogalarza@gmail.com>
2019-06-11 14:03:50 +0200

    UpdateAuth redux implementation

--- src/auth/reducers/index.js
+++ src/auth/reducers/index.js
@@ -17,14 +17,6 @@ export default (state = initialState, action) => {
         isValid: null,
         isFetching: true,
       };
-    case actionTypes.REQUEST_AUTH_SUCCESS:
-    case actionTypes.REQUEST_REGISTER_SUCCESS:
-      return {
-        ...state,
-        token: action.data,
-        isValid: true,
-        isFetching: false,
-      };
     case actionTypes.REQUEST_AUTH_ERROR:
     case actionTypes.REQUEST_REGISTER_ERROR:
       return {
@@ -33,6 +25,14 @@ export default (state = initialState, action) => {
         isValid: false,
         isFetching: false,
       };
+    case actionTypes.UPDATE_AUTH:
+        return {
+          ...state,
+          token: action.data.token,
+          id: action.data.id,
+          isValid: true,
+          isFetching: false,
+        };
     default:
       return state;
   }
